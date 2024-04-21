672f77c559e6b9e84a20168516b4969402e9abb5
RutamBhagat <rutambhagat@gmail.com>
2024-04-21 05:52:33 +0530

    finally the endpoint works

--- learning_langchain/routers/summarizer.py
+++ learning_langchain/routers/summarizer.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, status
+from fastapi.encoders import jsonable_encoder
 
 from learning_langchain.social_summarizer import social_summarizer
 from learning_langchain.utils.type_classes import Person_Request
@@ -6,6 +7,14 @@ from learning_langchain.utils.type_classes import Person_Request
 router = APIRouter()
 
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
+@router.post("/process", status_code=status.HTTP_201_CREATED)
 async def create_new_todo(person_request: Person_Request):
-    return social_summarizer(name=person_request.name)
+    person_info, profile_pic_url = social_summarizer(name=person_request.name)
+    response = {
+        "summary": person_info.summary,
+        "interesting_facts": person_info.interesting_facts,
+        "topics_of_interest": person_info.topics_of_interest,
+        "ice_breakers": person_info.ice_breakers,
+        "profile_picture": profile_pic_url,
+    }
+    return response
